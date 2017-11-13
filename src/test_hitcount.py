d0bf34ef1cd397677f8d61cbcc811f6cc3757a76
Abdelhak Marouane <33136280+amarouane-ABDLHAK@users.noreply.github.com>
2017-11-12 20:55:17 -0600

    Add three hits test

--- src/test_hitcount.py
+++ src/test_hitcount.py
@@ -8,16 +8,16 @@ class HitCountTest(unittest.TestCase):
         hitcount.hit("user1")
         self.assertEqual(b'1', hitcount.gethit("user1"))
     @patch('hitcount.r',mockredis.mock_strict_redis_client(host="0.0.0.0", port=6379, db=0))
-    def testTWOHit(self):
+    def testTWOHits(self):
         hitcount.hit("user1")
         hitcount.hit("user1")
         self.assertEqual(b'2', hitcount.gethit("user1"))
     @patch('hitcount.r',mockredis.mock_strict_redis_client(host="0.0.0.0", port=6379, db=0))
-    def testTWOHit(self):
+    def testThreeHits(self):
         hitcount.hit("user1")
         hitcount.hit("user1")
         hitcount.hit("user1")
-        self.assertEqual(b'32', hitcount.gethit("user1"))
+        self.assertEqual(b'3', hitcount.gethit("user1"))
 
 
 if __name__=='__main__':
