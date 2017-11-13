cfd822d6a2ae0957c9e4e25e6649190373c3395f
amarouane-ABDLHK <am0089@uah.edu>
2017-11-12 20:10:19 -0600

    addin new change

--- src/test_hitcount.py
+++ src/test_hitcount.py
@@ -12,6 +12,13 @@ class HitCountTest(unittest.TestCase):
         hitcount.hit("user1")
         hitcount.hit("user1")
         self.assertEqual(b'2', hitcount.gethit("user1"))
+   @patch('hitcount.r',mockredis.mock_strict_redis_client(host="0.0.0.0", port=6379, db=0))
+   def testTWOHit(self):
+        hitcount.hit("user1")
+        hitcount.hit("user1")
+        hitcount.hit("user1")
+        self.assertEqual(b'3', hitcount.gethit("user1"))
+
 
 if __name__=='__main__':
     unittest.main()
